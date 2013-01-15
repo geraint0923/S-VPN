41ebfbcb52eb7ae9655076fc09e9fef98ef0e360
Yang Yang <geraint0923@gmail.com>
2013-01-15 21:17:15 +0800

    Init Android S-VPN client

---
+++ client/android/S-VPN/src/com/thu/s_vpn/SVActivity.java
@@ -0,0 +1,22 @@
+package com.thu.s_vpn;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SVActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sv);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_sv, menu);
+		return true;
+	}
+
+}
